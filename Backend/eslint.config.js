778105266d64c113445499715cc7c70be6d2c561
Marcosz64 <marquigonzalezz22db@gmail.com>
2025-05-06 17:56:46 -0300

    Agregar carpeta Backend y ESlint

---
+++ Backend/eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js';
+import globals from 'globals';
+export default[
+    {
+        files: ['**/*.js'],
+        languageOptions:{
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            'no.unused-vars':'warn',
+        }
+    }
+];
